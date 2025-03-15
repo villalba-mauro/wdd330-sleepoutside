514ea88f4bba43593c64e23e812b81c915a3c70d
budujoseph <bud24002@byui.edu>
2025-03-15 00:17:31 +0000

    week 02-dynamic product lits, done with step 4

---
+++ src/js/ProdductList.mjs
@@ -0,0 +1,15 @@
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        try {
+            const list = await this.dataSource.getData();
+        } catch (error) {
+            console.error("Error fetching :", error);
+        }
+    }
+}
