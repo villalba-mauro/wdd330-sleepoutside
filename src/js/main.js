c9948dea96a7256d780b741c95a63d3b22fec64e
budujoseph <bud24002@byui.edu>
2025-03-20 21:02:40 +0000

    week 03 team activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
-
+import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+
+
 
 const listing = new ProductList("Tents", dataSource, element);
 
+loadHeaderFooter();
+
 listing.init();
+
 
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -45,3 +45,40 @@ export function renderListWithTemplate(
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+export function renderWithTemplate(
+  template,
+  parentElement,
+  data,
+  callback,
+) {
+  // const htmlStrings = list.map(templateFn);
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+//   if (clear) {
+//     parentElement.innerHTML = "";
+//   }
+//   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template; 
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+
+  const headerElement = document.querySelector("#main-header");
+
+  renderWithTemplate(headerTemplate, headerElement);
+
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(footerTemplate, footerElement);
+}
